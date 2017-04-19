878764bd2c5e9faf238f07695249bb47ebd5a736
Olaf Alders <olaf@wundersolutions.com>
2017-04-18 21:58:20 -0400

    Change default layout.

---
+++ bin/update-assets.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp ../the-project-bootstrap-template/html/template/bootstrap/css/bootstrap.css public/assets/css/
+cp ../the-project-bootstrap-template/html/template/css/style.css public/assets/css/
+cp ../the-project-bootstrap-template/html/template/fonts/font-awesome/css/font-awesome.css public/assets/css/
+cp ../the-project-bootstrap-template/html/template/fonts/fontello/css/fontello.css public/assets/css/
+cp ../the-project-bootstrap-template/html/template/css/typography-default.css public/assets/css/
+cp ../the-project-bootstrap-template/html/template/plugins/jquery.min.js public/assets/js/
+cp ../the-project-bootstrap-template/html/template/bootstrap/js/bootstrap.min.js public/assets/js/
