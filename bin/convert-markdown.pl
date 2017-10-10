92fc52e4c8235cea02452171017fd4541432693b
Olaf Alders <olaf@wundersolutions.com>
2017-10-10 19:10:14 -0400

    Use Pandoc for markdown to HTML conversion

--- bin/convert-markdown.pl
+++ bin/convert-markdown.pl
@@ -5,22 +5,26 @@ use warnings;
 use feature qw( say );
 
 use Path::Tiny qw( path );
-use Text::Markdown qw( markdown );
 
 path('templates/html/answers')->mkpath;
 
 my @templates = glob('templates/markdown/answers/*.md');
 
 foreach my $file (@templates) {
-    my $content = path($file)->slurp;
-
     my $html_file_path = $file;
     $html_file_path =~ s{markdown}{html};
     $html_file_path =~ s{\.md}{.html};
 
-    path($html_file_path)->spew( markdown($content) );
+    my @args = ( 'pandoc', '-f', 'markdown', '-t', 'html', $file, '-o', $html_file_path);
+    system( @args );
+
+    # pandoc inserts a UTF-8 character which appears to get double encoded when
+    # Mojo spits it out.  Just use an HTML entity rather than trying to figure
+    # this out.
+    `perl -pi -e 's/↩/&#8617;/gc' $html_file_path`;
     say "Created/updated $html_file_path";
 }
+
 
 # PODNAME: convert-markdown.pl
 # ABSTRACT: convert markdown files to HTML
